fa6e5ea8a4fd55628d7b4f27aeaf71d9824bb315
valdirnerin <valdirnerin@hotmail.com>
2025-10-10 18:31:54 -0300

    feat: force dynamic rendering on prisma pages

--- nerin-electric-site-v3-fixed/app/empresa/page.tsx
+++ nerin-electric-site-v3-fixed/app/empresa/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import Image from 'next/image'
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
