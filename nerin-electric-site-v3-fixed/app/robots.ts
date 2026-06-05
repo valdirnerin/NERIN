4b5b0377074eeb641fa701f1725316bbe06d8547
valdirnerin <valdirnerin@hotmail.com>
2026-06-05 16:05:28 -0300

    Disallow private routes in robots

--- nerin-electric-site-v3-fixed/app/robots.ts
+++ nerin-electric-site-v3-fixed/app/robots.ts
@@ -1,11 +1,29 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://www.nerin.com.ar'
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, '') ||
+    process.env.SITE_URL?.replace(/\/$/, '') ||
+    'https://www.nerin.com.ar'
+
   return {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: [
+        '/admin',
+        '/admin/',
+        '/clientes',
+        '/clientes/',
+        '/tecnico',
+        '/tecnico/',
+        '/api/admin',
+        '/api/admin/',
+        '/api/quotes',
+        '/api/quotes/',
+        '/api/upload/project-photo',
+        '/api/upload/project-photo/',
+      ],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   }
