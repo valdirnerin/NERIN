969f647e5f06fcf8f3e50f65f6e152711ce41500
valdirnerin <valdirnerin@hotmail.com>
2026-07-06 08:38:37 -0300

    Load electrical admin content state

--- nerin-electric-site-v3-fixed/app/(admin)/admin/(shell)/contenido/trabajos-electricos/page.tsx
+++ nerin-electric-site-v3-fixed/app/(admin)/admin/(shell)/contenido/trabajos-electricos/page.tsx
@@ -1,10 +1,10 @@
-import { getElectricalAdminContent } from '@/lib/electrical-admin-content'
+import { getElectricalAdminContentState } from '@/lib/electrical-admin-content'
 import { ElectricalContentManager } from '../../trabajos-electricos/ElectricalContentManager'
 
 export const dynamic = 'force-dynamic'
 export const runtime = 'nodejs'
 
 export default async function AdminContenidoTrabajosElectricosPage() {
-  const content = await getElectricalAdminContent()
-  return <ElectricalContentManager initialContent={content} />
+  const state = await getElectricalAdminContentState()
+  return <ElectricalContentManager initialState={state} />
 }
